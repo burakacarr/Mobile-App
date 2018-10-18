734b6f9440b24d7de8b47641f93988ebf4c942ca
Burak Acar <nburakacar@gmail.com>
2018-10-18 23:21:20 +0300

    TravelBook

---
+++ TravelBook/app/src/main/java/com/example/acar/travelbook/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.acar.travelbook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_place,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.add_place){
+            //diğer sayfaya gideceğiz
+            Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+    }
+}
