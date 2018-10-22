f1a30b014559a3998ca63a2f5f232ef2176aee09
Burak Acar <nburakacar@gmail.com>
2018-10-22 19:11:52 +0300

    ParseLearning

---
+++ ParseLearning/app/src/main/java/com/example/acar/parselearning/ParseStarterClass.java
@@ -0,0 +1,26 @@
+package com.example.acar.parselearning;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseStarterClass extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+        .applicationId("")
+        .clientKey("")
+        .server("")
+        .build()
+        );
+
+
+
+    }
+}
